9c192fe984093b42b295b35726dfc4b278aa8e8e
khiuta <minhaepic1551@gmail.com>
2025-11-30 16:43:14 -0300

    colocando cmake

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/funcao.cpp.o"
+  "CMakeFiles/main.dir/src/funcao.cpp.o.d"
+  "CMakeFiles/main.dir/src/isolamento.cpp.o"
+  "CMakeFiles/main.dir/src/isolamento.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/quadro_resumo.cpp.o"
+  "CMakeFiles/main.dir/src/quadro_resumo.cpp.o.d"
+  "CMakeFiles/main.dir/src/refinamento.cpp.o"
+  "CMakeFiles/main.dir/src/refinamento.cpp.o.d"
+  "CMakeFiles/main.dir/src/tratamento.cpp.o"
+  "CMakeFiles/main.dir/src/tratamento.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
