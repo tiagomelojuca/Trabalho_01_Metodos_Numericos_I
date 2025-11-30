e59988fcfd4d7a0bdeaab28870aa2fe6eeeb17c4
khiuta <minhaepic1551@gmail.com>
2025-11-30 17:57:29 -0300

    mudanças e cmake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/funcao.cpp" "CMakeFiles/main.dir/src/funcao.cpp.o" "gcc" "CMakeFiles/main.dir/src/funcao.cpp.o.d"
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/isolamento.cpp" "CMakeFiles/main.dir/src/isolamento.cpp.o" "gcc" "CMakeFiles/main.dir/src/isolamento.cpp.o.d"
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/quadro_resumo.cpp" "CMakeFiles/main.dir/src/quadro_resumo.cpp.o" "gcc" "CMakeFiles/main.dir/src/quadro_resumo.cpp.o.d"
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/refinamento.cpp" "CMakeFiles/main.dir/src/refinamento.cpp.o" "gcc" "CMakeFiles/main.dir/src/refinamento.cpp.o.d"
+  "/home/poguel/Documents/Trabalho_01_Metodos_Numericos_I/src/tratamento.cpp" "CMakeFiles/main.dir/src/tratamento.cpp.o" "gcc" "CMakeFiles/main.dir/src/tratamento.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
